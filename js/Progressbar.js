44003a3f270c24fe22dc372acfce7e54a9413ed2
balaji1832 <balaji49034@gmail.com>
2025-10-09 14:54:31 +0530

    Carousel-slides Added

---
+++ js/Progressbar.js
@@ -0,0 +1,6 @@
+window.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY;
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercent = (scrollTop / docHeight) * 100;
+    document.getElementById("scroll-progress").style.width = scrollPercent + "%";
+  });
